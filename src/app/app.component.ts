4de30011d337cff26537daabbc9d2edc2291e370
Ganesh Prabhakar mulik <129154209+ganesh-mulik@users.noreply.github.com>
2024-07-22 17:35:41 +0530

    structural and component directives learned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo-for-practise';
   isActive = true;
+  backgroundColor = 'orange';
+  fontSize = 30;
+  name = "value";
+  // variables for structural directives
+  first = 10;
+  second = 8;
+  tableOfTwo = [2,4,6,8,10,12,14,16,18,20];
+  dept = 'mca';
 
   toggleActive(){
     this.isActive=!this.isActive;
